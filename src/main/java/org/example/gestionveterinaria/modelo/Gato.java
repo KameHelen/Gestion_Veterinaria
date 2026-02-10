29fa42509e398480b880721712632f9b63ce98fb
Martarl <martaramirezlopez78@alumnos.ilerna.com>
2026-02-10 19:18:48 +0100

    Subiendo Perro

--- src/main/java/org/example/gestionveterinaria/modelo/Gato.java
+++ src/main/java/org/example/gestionveterinaria/modelo/Gato.java
@@ -1,4 +1,15 @@
 package org.example.gestionveterinaria.modelo;
 
-public class Gato {
+public class Gato extends Animal {
+
+    public Gato (String numero_chip, String nombre, String fecha_nacimiento, Cliente dueno) {
+        super (numero_chip, nombre, fecha_nacimiento, dueno);
+    }
+
+
+    @Override
+    public String getTipo() {
+        return "Gato";
+    }
+
 }
--- src/main/java/org/example/gestionveterinaria/modelo/Perro.java
+++ src/main/java/org/example/gestionveterinaria/modelo/Perro.java
@@ -1,4 +1,15 @@
 package org.example.gestionveterinaria.modelo;
 
-public class Perro {
+public class Perro extends Animal {
+
+    public Perro (String numero_chip, String nombre, String fecha_nacimiento, Cliente dueno) {
+        super (numero_chip, nombre, fecha_nacimiento, dueno);
+    }
+
+
+    @Override
+    public String getTipo() {
+        return "Perro";
+    }
+
 }
